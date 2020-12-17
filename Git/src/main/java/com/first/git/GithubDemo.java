9e8d73af1da99aaee5c0fbe628bbb421177a5603
dnyane999 <76145230+dnyane999@users.noreply.github.com>
2020-12-17 15:22:58 +0530

    Update GithubDemo.java

--- Git/src/main/java/com/first/git/GithubDemo.java
+++ Git/src/main/java/com/first/git/GithubDemo.java
@@ -2,6 +2,11 @@ package com.first.git;
 
 public class GithubDemo 
 {
+  public m1()
+  {
+  System.out.println("m---method");	
+
+  }
 public static void main(String[] args)
 {
 System.out.println("main----method");	
